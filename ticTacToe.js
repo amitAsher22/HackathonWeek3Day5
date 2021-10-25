2911e73971261a256ac8ec9b498f21b881736fba
amita <amitasher4@gmail.com>
2021-10-25 11:50:35 +0300

    first commit

---
+++ ticTacToe.js
@@ -0,0 +1,98 @@
+
+let X_CLASS = 'x';
+let CIRCLE_CLASS = 'circle';
+let WINNING_COMBNATION = [
+    [0 , 1, 2],
+    [3, 4, 5],
+    [6 ,7 ,8],
+    [0 ,3 , 6],
+    [1 ,4 ,7],
+    [2 , 5, 8],
+    [0 , 4 ,8],
+    [2, 4, 6]
+]
+let cellElements = document.querySelectorAll('[data-cell]');
+let board = document.getElementById('board');
+let winningMessageElement = document.getElementById('winningMessage');
+let restartButton = document.getElementById('restartButton')
+let circleTurn  
+let winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+
+ ///
+ startGame()
+
+
+ restartButton.addEventListener("click" , startGame)
+
+function handleClick(event){
+    let cell = event.target
+    let currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell , currentClass)
+    if(checkWin(currentClass)){
+       endGame(false)
+
+    }else if(isDraw()){
+        endGame(true)
+    }else{
+        swapTurn()
+        setBoardHoverClass()
+    }
+   
+}
+
+function endGame(draw){
+  if(draw){
+    winningMessageTextElement.innerText = 'Draw!'
+  }else{
+      winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} wins!`
+  }
+  winningMessageElement.classList.add('show')
+}
+
+function startGame(){
+    circleTurn = false
+    cellElements.forEach(cell =>{
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCLE_CLASS)
+        cell.removeEventListener('click', handleClick   )
+        cell.addEventListener("click" , handleClick , {once:true})
+    })
+    setBoardHoverClass()
+    winningMessageElement.classList.remove('show')
+}
+
+function isDraw(){
+    return [...cellElements].every(cell =>{
+        cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+    })
+}
+
+
+function placeMark (cell , currentClass){
+  cell.classList.add(currentClass)
+}
+
+
+function swapTurn(){
+    circleTurn = !circleTurn
+}
+
+function setBoardHoverClass(){
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
+   if(circleTurn){
+       board.classList.add(CIRCLE_CLASS)
+   }else{
+    board.classList.add(X_CLASS)
+   }
+}
+
+function checkWin(currentClass ){
+   return WINNING_COMBNATION.some(combination =>{
+       return combination.every(index =>{
+           return cellElements[index].classList.contains(currentClass)
+       })
+   })
+}
+////
+
